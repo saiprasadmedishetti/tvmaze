7a07ecffc0383229dbd4848f6ebafff313bfb8d6
saiprasadmedishetti <saiprasad.m@quillhash.com>
2021-02-16 10:25:41 +0530

    added base route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Favorites from "../components/Favorites/Favorites";
 import Shows from "../components/Shows/Shows";
 
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Shows} />
+      <Route exact path="/" component={Shows}>
+        <Redirect to="/shows" />
+      </Route>
       <Route exact path="/shows" component={Shows} />
       <Route exact path="/favorites" component={Favorites} />
     </Switch>
