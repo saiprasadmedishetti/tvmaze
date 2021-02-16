6ea9c7cfa5b19b6bf09c7091ac9c943bad034a65
saiprasadmedishetti <saiprasad.m@quillhash.com>
2021-02-16 10:19:53 +0530

    added base route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/tvmaze">
         <App />
       </Router>
     </Provider>
